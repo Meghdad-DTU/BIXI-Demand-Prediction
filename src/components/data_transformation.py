9eccb91c9c552f96783368618a008e940a11cb3d
Meghdad <mehdizadeh.iust@gmail.com>
2023-07-12 16:23:34 +0000

    Updating utils

---
+++ src/components/data_transformation.py
@@ -0,0 +1,80 @@
+import os
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
+
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+    
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+        
+    def get_data_transformer_object(self):
+        '''
+        This funnction is responsible for data transformation
+        '''
+        try:
+            community_demand_columns = ['com1',	'com2',	'com3', 'com4', 'com5',	'com6']
+            demand_pipeline = Pipeline(
+                steps = [
+                    'scaler', MinMaxScaler(feature_range=(-1, 1))
+                    ]
+            )
+            logging.info(f'Community demand features:{community_demand_columns}')
+            
+            preprocessor = ColumnTransformer(
+                [
+                'demand_pipeline', demand_pipeline, community_demand_columns    
+                ]
+            )
+            
+            return preprocessor
+                
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def initiate_data_transformatio(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+                
+            logging.info('Read train and test data completed')
+                
+            logging.info("Obtaining preprocessing object")
+            preprocessing_obj=self.get_data_transformer_object()
+            
+            feature_columns = ['com1',	'com2',	'com3', 'com4', 'com5',	'com6']
+            input_feature_train_df = train_df[feature_columns]
+            input_feature_test_df = test_df[feature_columns]
+                
+            logging.info(f"Applying preprocessing object on both train and test dataframes")
+            train_arr = preprocessing_obj.transform(input_feature_train_df)
+            test_arr = preprocessing_obj.transform(input_feature_test_df)
+            
+            logging.info('Saved preprocessing object')
+            save_object(
+                file_path= self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj)
+            
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+            
+                
+        except Exception as e:
+            raise CustomException(e, sys)
+            
+    
+    
--- src/utils.py
+++ src/utils.py
@@ -12,8 +12,57 @@ def save_object(file_path, obj):
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
         
-        with open(file_path, 'wb' as file_obj):
+        with open(file_path, 'wb') as file_obj:
             pickle.dump(obj, file_obj)
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+        
+        
+def split_sequence(sequence, lag):
+    '''
+    This function splits a given univariate sequence into
+    multiple samples where each sample has a specified number
+    of time steps and the output is a single time step.
+    '''
+    try:
+        X, y = list(), list()
+        for i in range(len(sequence)):
+        # find the end of this pattern
+            end_ix = i + lag
+
+        # check if we are beyond the sequence
+            if end_ix > len(sequence)-1:
+                break
+        # gather input and output parts of the pattern
+            seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+            X.append(seq_x)
+            y.append(seq_y)
+        return np.array(X), np.array(y)
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def convert_to_supervised(dat, lag):
+    '''
+    This function takes a 2D sequennce, scales the array and splits
+    a given multivariate sequence into multiple samples where each sample has a specified number
+    of time steps. It returns multiple time steps as well as the scaler.
+    param dat (DataFrame): Bike sharing demand for each community over time
+    param lag (int): History length or time lag
+    '''
+    
+    try:
+        m, n = dat.shape
+        # e.g., if lag = 7, BIXI demand of past 7*15 minutes
+        X = np.zeros((m-lag,lag, n))
+        Y = np.zeros((m-lag,n))
+
+        for i in range(0,n):
+            x, y = split_sequence(dat[:,i],lag)
+            X[:,:,i] = X
+            Y[:,i] = y
+        return X, Y
     
     except Exception as e:
         raise CustomException(e, sys)
